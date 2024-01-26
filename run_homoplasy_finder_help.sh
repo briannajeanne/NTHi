c34cd966e52d79216617a4fad291f41cbc3a465e
briannajeanne <37121973+briannajeanne@users.noreply.github.com>
2024-01-26 10:22:58 -0500

    Add files via upload

---
+++ run_homoplasy_finder_help.sh
@@ -0,0 +1,6 @@
+#java -jar /home/bbixler/HomoplasyFinder.jar \
+# --tree /home/bbixler/Hflu/bactopia-tools/tmp_panC1C2context/binary_presence_absence.nwk\
+# --presenceAbsence /home/bbixler/Hflu/bactopia-tools/tmp_panC1C2context/binary_reformat_noNA.csv --createAnnotatedTree
+
+./homoplasy_finder_help.sh /home/bbixler/HomoplasyFinder.jar /home/bbixler/Hflu/bactopia-tools/tmp_panC1C2context/binary_presence_absence.nwk /home/bbixler/Hflu/bactopia-tools/tmp_panC1C2context/binary_reformat_noNA.csv
+
